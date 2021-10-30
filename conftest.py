209979175b56c7131ea34b92da7020a666c9b449
ElinaGorniker <elinagorniker@gmail.com>
2021-10-30 20:07:57 +0200

    configuration of test environment and creating common classes for main page

---
+++ conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help='Choose language: ar, ca, cs, da, de, el, en, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans, en-gb')
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ test_main_page.py
@@ -0,0 +1,7 @@
+from pages.main_page import MainPage
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # initialize the Page Object, pass the driver instance and url to the constructor
+    page.open()  # open page
+    page.go_to_login_page()  # execute the page method — go to the login page
